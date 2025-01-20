e07120753f40b9f720ccbdea37e05641bbe3ac11
Romain CORNUAULT <rougefox85@gmail.com>
2025-01-20 15:39:20 +0100

    ✨ Add "Exemplaires" modifying

--- src/main/java/fr/eni/ludotheque/controllers/ExemplaireController.java
+++ src/main/java/fr/eni/ludotheque/controllers/ExemplaireController.java
@@ -5,19 +5,19 @@ import fr.eni.ludotheque.bll.ExemplaireService;
 import fr.eni.ludotheque.bll.JeuService;
 import fr.eni.ludotheque.bo.Client;
 import fr.eni.ludotheque.bo.Exemplaire;
+import fr.eni.ludotheque.bo.Genre;
 import fr.eni.ludotheque.bo.Jeu;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequestMapping("/exemplaires")
 @Controller
@@ -57,7 +57,32 @@ public class ExemplaireController {
         exemplaire.setJeu(jeuAssocie.get());
         exemplaireService.save(exemplaire);
 
+        if(exemplaire.getNoExemplaire() != null){
+            return "redirect:/jeux/" + exemplaire.getJeu().getNoJeu();
+        }
         return "redirect:/exemplaires/ajouter";
+    }
+
+    @GetMapping("/modifier/{noExemplaire}")
+    public String getModifierClient(@PathVariable(name="noExemplaire") int noExemplaire, Model modele) {
+        Optional<Exemplaire> exemplaireOpt = exemplaireService.findById(noExemplaire);
+
+        if (exemplaireOpt.isPresent()) {
+            Exemplaire exemplaire = exemplaireOpt.get();
+            Optional<Jeu> jeuAssocie = jeuService.findById(exemplaire.getJeuId());
+            modele.addAttribute("exemplaire", exemplaire);
+
+            if(jeuAssocie.isPresent()){
+                System.out.println(jeuAssocie.get());
+                List<Jeu> jeux = new ArrayList<Jeu>();
+                jeux.add(jeuAssocie.get());
+                modele.addAttribute("jeux", jeux);
+            }
+
+            return "exemplaire/formulaire-exemplaire";
+        }
+
+        return "redirect:/jeux";
     }
 
 }
